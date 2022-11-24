7e8155d3071e2e13b6b0595354c30f30f751468f
EthanAbra <70662211+EthanAbra@users.noreply.github.com>
2022-11-23 20:22:57 -0500

    change app run

--- runserver.py
+++ runserver.py
@@ -16,7 +16,7 @@ def main(argv):
         print('Port must be an integer.', file=stderr)
         exit(1)
 
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
 
 if __name__ == '__main__':
     main(argv)
