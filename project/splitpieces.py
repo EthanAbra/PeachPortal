a6f4188c51e327afcf017c900da174bb0d6b3493
EthanAbra <ea10@princeton.edu>
2022-12-24 23:56:58 -0800

    nicer format, want tab coloring

--- project/splitpieces.py
+++ project/splitpieces.py
@@ -1,7 +1,7 @@
 import numpy as np
 from bokeh.plotting import figure, show
 from bokeh.io import output_notebook, curdoc
-from bokeh.layouts import layout, row, column, Spacer
+from bokeh.layouts import layout, row, column, Spacer, gridplot, grid
 from bokeh.models import CustomJS, RangeSlider, BoxAnnotation, Button, Dropdown, TextInput
 from bokeh.application import Application
 from bokeh.application.handlers import FunctionHandler
@@ -9,6 +9,7 @@ from .database import queryUnsplitData
 import os
 from dotenv import load_dotenv
 import pymongo
+from bokeh.models import Tabs, TabPanel
 
 
 load_dotenv()
@@ -19,6 +20,18 @@ else:
 bokehdb = pymongo.MongoClient(CONNECTION_STRING).peach
 
 
+def my_insort_left(a, x, lo=0, hi=None):
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if int(a[mid].name.split(',')[1]) < int(x.name.split(',')[1]): lo = mid+1
+        else: hi = mid
+    a.insert(lo, x)
+
+
 def my_gui(doc):
     args = doc.session_context.request.arguments
     unsplitId = int(args.get('id')[0])
@@ -27,19 +40,6 @@ def my_gui(doc):
     athlete_map = elite.athlete_map
     if len(athlete_map)==0:
         athlete_map = ['seat ' + str(i) for i in range(1,9)]
-    # Main figure
-    
-    def my_insort_left(a, x, lo=0, hi=None):
-        if lo < 0:
-            raise ValueError('lo must be non-negative')
-        if hi is None:
-            hi = len(a)
-        while lo < hi:
-            mid = (lo+hi)//2
-            if int(a[mid].name.split(',')[1]) < int(x.name.split(',')[1]): lo = mid+1
-            else: hi = mid
-        a.insert(lo, x)
-    
     
     stroke_nums = np.arange(elite.numstrokes)
     boat_pow = elite.get_boat_power()
@@ -60,79 +60,93 @@ def my_gui(doc):
     p2.toolbar.active_tap = None
 
     # Slider widget
-    rslider = RangeSlider(start=1, end=elite.numstrokes, value=(1,elite.numstrokes),
+    rslider = RangeSlider(start=1, end=elite.numstrokes, value=(1,elite.numstrokes), bar_color = "#0398fc",
                               title=None,show_value=False, height = 50, height_policy = "fixed", sizing_mode = "stretch_width", margin = (45,10,50,0))
     spacer_edit = Spacer(width=40)
     
-    make_piece = Button(label="Make Piece", button_type="success",height = 100, width = 100, height_policy = "fixed", margin = (0,0,50,0))
+    make_piece = Button(label="Make Piece", button_type="success",height = 100, width = 100, height_policy = "fixed", margin = (0,0,500,0))
     
     
     def toggleRemoveCallback(attr):
-        rootLayout = doc.get_model_by_name('mainLayout')
-        listOfSubLayouts = rootLayout.children
-        tryname = doc.get_model_by_name('crop,' + str(attr.model.name))
+        rootLayout = doc.get_model_by_name('rootLayout')
+        listOfSubLayouts = rootLayout.children[-2].children.copy()
+        listOfSubLayouts = [x[0] for x in listOfSubLayouts]
+        tryname = doc.get_model_by_name('tabs,' + str(attr.model.name))
         trycbox = doc.get_model_by_name('cbox,' + str(attr.model.name))
         trymbox = doc.get_model_by_name('mbox,' + str(attr.model.name))
         listOfSubLayouts.remove(tryname)
-        if len(listOfSubLayouts) == 4:
+        rootLayout.children[-2] = gridplot(children = listOfSubLayouts, ncols=2, toolbar_location = None)
+        if len(listOfSubLayouts) == 0:
             tryconf = doc.get_model_by_name('confirm,' + str(elite.numstrokes*10))
-            listOfSubLayouts.remove(tryconf)
-        listOfSubLayouts[1].center.remove(trycbox)
-        listOfSubLayouts[0].center.remove(trymbox)
+            tryconf.visible = False
+        rootLayout.children[1].center.remove(trycbox)
+        rootLayout.children[0].center.remove(trymbox)
         
         
     def toggleConfirmCallback(attr):
-        rootLayout = doc.get_model_by_name('mainLayout')
-        listOfSubLayouts = rootLayout.children
+        rootLayout = doc.get_model_by_name('rootLayout')
+        listOfSubLayouts = rootLayout.children[-2].children.copy()
+        listOfSubLayouts = [x[0] for x in listOfSubLayouts]
         pieceArr = []
-        for item in listOfSubLayouts[3:-1]:
+        for slitem in listOfSubLayouts:
+            item = slitem._property_values['tabs'][1]._property_values['child']._property_values['children']
             retDict = {}
-            retDict['title'] = item.children[-1].children[0].value
-            retDict['athletemap'] =  [x.value for x in item.children[-1].children[1:]]
+            retDict['title'] = item[1].value
+            retDict['athletemap'] =  [x[0].value for x in item[2]._property_values['children']]
             pieceArr.append(retDict)
         print(pieceArr)
         
         
-    # Set the callback for the toggle button
-
-    
     def toggleMakeCallback(attr):
         # Get the layout object added to the documents root
-        rootLayout = doc.get_model_by_name('mainLayout')
-        listOfSubLayouts = rootLayout.children
+        rootLayout = doc.get_model_by_name('rootLayout')
         
-            
+        listOfSubLayouts = rootLayout.children[-2].children.copy()
         
         crop_start = int(box.left)
         crop_end = int(box.right)
         
-        tryname = doc.get_model_by_name('crop,' + str(crop_start) + "," + str(crop_end))
+        tryname = doc.get_model_by_name('tabs,' + str(crop_start) + "," + str(crop_end))
         
         if tryname is not None:
+            print('tnnamenotnone')
             return
         
-        text_inputs = [TextInput(value=athlete, title="Seat " + str(athIdx+1) + ":") for athIdx, athlete in enumerate(athlete_map)]
-        title_input = TextInput(value = "Strokes " + str(crop_start)+'-'+str(crop_end), title = "Piece Name:")
-        text_inputs.insert(0,title_input)
-        rmv_piece = Button(label="Remove Piece", button_type="danger", name = str(crop_start) + "," + str(crop_end))
+        
+        text_inputs = [TextInput(value=athlete, title="Seat " + str(athIdx+1) + ":",  
+                                 name = 'tinp,' + str(crop_start) + "," + str(crop_end)) for athIdx, athlete in enumerate(athlete_map)]
+        title_input = TextInput(value = "Strokes " + str(crop_start)+'-'+str(crop_end), align="center",
+                                title = "Piece Name:", name = 'tinp,' + str(crop_start) + "," + str(crop_end))
+        rmv_piece = Button(label="Remove Piece", button_type="danger", name = str(crop_start) + "," + str(crop_end), width_policy='max', height = 60, height_policy = "fixed")
         rmv_piece.on_click(toggleRemoveCallback)
         
-        p3 = figure()
+        p3 = figure(min_border = 0)
         plotToAdd = p3
         p3.line(stroke_nums[crop_start:crop_end+1],boat_pow[crop_start:crop_end+1])
-        # listOfSubLayouts.append()
-        my_insort_left(listOfSubLayouts, row(plotToAdd,column([Spacer(height=60) for i in range(len(text_inputs)//2)] + [rmv_piece]), column(text_inputs), name='crop,' + str(crop_start) + "," + str(crop_end),  margin = (105,0,-105,0))) 
+        tab1 = TabPanel(child=p3, title="piece")
+
+        p5 = column(rmv_piece, title_input, grid(text_inputs, ncols = 2))
+        
+        tab2 = TabPanel(child=p5, title="piece info")
+
+        tabs = Tabs(tabs=[ tab1, tab2 ], name = 'tabs,' + str(crop_start)+','+str(crop_end), margin = (105,0,-105,0))
+        
+        listOfSubLayouts = [x[0] for x in listOfSubLayouts]
+        
+        my_insort_left(listOfSubLayouts, tabs) 
+        
+        rootLayout.children[-2] = gridplot(children = listOfSubLayouts, ncols=2, toolbar_location = None)
+            
         cropbox = BoxAnnotation(fill_alpha=0.5, line_alpha=0.5, level='underlay', 
                                 fill_color = 'green', left=crop_start, right=crop_end, name='cbox,' +  str(crop_start) + "," + str(crop_end))
         p2.add_layout(cropbox)
         mainbox = BoxAnnotation(fill_alpha=0.1, line_alpha=0.5, level='underlay', 
                                 fill_color = 'green', left=crop_start, right=crop_end, name='mbox,' +  str(crop_start) + "," + str(crop_end))
         p1.add_layout(mainbox)
+        tryconf = doc.get_model_by_name('confirm,' + str(elite.numstrokes*10))
+        tryconf.visible = True
         
-        if len(listOfSubLayouts)==4:
-            confirm_button = Button(label="Confirm Pieces Creation", button_type="success", width_policy='max', height = 60, height_policy = "fixed", name = 'confirm,' + str(elite.numstrokes*10), margin=(115,0,5,0))
-            confirm_button.on_click(toggleConfirmCallback)
-            my_insort_left(listOfSubLayouts, confirm_button)
+
         
     # Set the callback for the toggle button
     make_piece.on_click(toggleMakeCallback)
@@ -160,8 +174,13 @@ def my_gui(doc):
     # Layout
     p2.add_layout(box)
     
-    #, row(cropped_piece, selected_crop)
-    mainLayout = layout(p1, p2, row(spacer_edit,rslider, make_piece, name = 'slider,-1,0'), name='mainLayout', sizing_mode="scale_both")
-    
-    doc.add_root(mainLayout)
+    rootLayout = layout(p1, p2, row(spacer_edit,rslider, make_piece, name = 'slider,-1,0'), row(Spacer(height = 5000)), name='rootLayout', sizing_mode="scale_both")
+    mainLayout = gridplot(children = [], ncols=2)
+    rootLayout.children.append(mainLayout)
+    confirm_button = Button(label="Confirm Pieces Creation", button_type="success", width_policy='max',
+                            height = 60, height_policy = "fixed", visible = False, 
+                            name = 'confirm,' + str(elite.numstrokes*10), margin=(115,0,5,0))
+    confirm_button.on_click(toggleConfirmCallback)
+    rootLayout.children.append(confirm_button)
+    doc.add_root(rootLayout)
     return doc
